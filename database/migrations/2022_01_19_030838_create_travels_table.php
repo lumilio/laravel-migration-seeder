8d8ffe00ff9b0fd2062e24cb3ed01e8d1d3ac1fd
lumilio <logistic-division@lumilium.com>
2022-01-19 06:54:50 +0100

    #

--- database/migrations/2022_01_19_030838_create_travels_table.php
+++ database/migrations/2022_01_19_030838_create_travels_table.php
@@ -16,7 +16,7 @@ class CreateTravelsTable extends Migration
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->date('start_trip')->nullable();
+            $table->date('start_trip')->nullable()->format('Y-m-d H:i:s');
             $table->string('city1')->nullable();
             $table->string('city2')->nullable();
             $table->decimal('price',10,2)->nullable();
