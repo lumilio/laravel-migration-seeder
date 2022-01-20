422b7209218516130c80ad144c4f51b6520fd2b7
lumilio <logistic-division@lumilium.com>
2022-01-20 05:17:23 +0100

    Udate arrey data on seeders

--- database/seeds/PostlSeeder.php
+++ database/seeds/PostlSeeder.php
@@ -15,28 +15,28 @@ class PostlSeeder extends Seeder
         $posts = [
             [
                 'author' => 'Giovanni Beluga',
-                'photo' => '',
-                'text' => '',
+                'photo' => 'https://www.impiegatagiramondo.it/wp-content/uploads/2020/01/capodanno-alle-maldive.jpg',
+                'text' => 'Buone feste !!!',
             ],
             [
                 'author' => 'Marco Aurelio',
-                'photo' => '',
+                'photo' => 'https://www.parigi.it/images/large/tbgc17487_parigiit-guida-di-parigi4.jpeg?v=1da1',
                 'text' => '',
             ],
             [
                 'author' => 'Diego Rossi',
-                'photo' => '',
-                'text' => '',
+                'photo' => 'https://i0.wp.com/passporterapp.com/it/blog/wp-content/uploads/2021/04/que-ver-en-cuba.jpg?fit=1994%2C1438&ssl=1',
+                'text' => 'eccovi una foto da cuba',
             ],
             [
                 'author' => 'Lorenzo Mari',
-                'photo' => '',
-                'text' => '',
+                'photo' => 'https://mobilita.org/wp-content/uploads/2021/11/parigi.jpg',
+                'text' => 'che vacanza straordinaria',
             ],
             [
                 'author' => 'Enrico Gualandi',
-                'photo' => '',
-                'text' => '',
+                'photo' => 'https://scontent.ccdn.cloud/image/vivitravels-it/f9bcee6c-6a01-47ba-b53e-531a76c11170/maxw-960.jpg',
+                'text' => 'sono soddisfatto di aver fatto questo viaggio',
             ],
         ];
 
